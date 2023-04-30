2d8432a03d7bb8913168c6a43b1ee26d7685c15f
Ethan Romm <ejromm@gmail.com>
2023-04-30 14:54:05 -0400

    added about info

--- src/pages/About.js
+++ src/pages/About.js
@@ -199,9 +199,8 @@ S........::..,.:.......,S@*%@#?@@@;.........................,,,:?##%???*:+;:,;?+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,.....................................:;;:,...........,:::::::::::::::::::,<br/></pre>
 
 
-<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non prident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-<span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-</span>
+<span>Ethan Romm is a multimedia artist working mainly in photography and filmmaking. Born and raised in Arlington, VA, He currently resides in Richmond, VA and is a recent graduate of film at Virginia Commonwealth University.</span>
+<span>His work, across all forms, focuses on growing up, introspection, and interpersonal relationships. He aims to distill the good and bad, but mostly bad qualities of American culture and human nature into his work.</span>
  </div>
     )
 }
