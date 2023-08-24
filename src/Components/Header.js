7a115c71663344507504af792591a42b8b7a7a19
Ethan Romm <ejromm@gmail.com>
2023-08-24 13:57:55 -0400

    responsive navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,25 @@
-import React from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import '../styles/Header.css'; 
-import NavBar from './NavBar';
-export default function Header() {
+import { Link } from 'react-router-dom'; 
 
+export default function Header() {
+    const [navExtended, setNavExtended] = useState(false); 
+    
 
     return (
         <>
          <div className='header'>
-          <span>Ethan Romm</span>
-          <NavBar />
+          <Link className='header-name'  to={'/Senior-Portfolio-Page/'}page='home'>Ethan Romm</Link>
+          <button className={navExtended ? 'open-navbar-btn active' :'open-navbar-btn'} onClick={() => setNavExtended(!navExtended)}>
+             <span className='line-1'></span>
+             <span className='line-2'></span>
+             <span className='line-3'></span>
+          </button>
+          <nav  onClick={() => setNavExtended(false)} className={navExtended ? 'nav-open' : ''}>
+          <Link to={'/Senior-Portfolio-Page/film'} onClick={() => setNavExtended(!navExtended)} page='film'>film</Link>
+          <Link to={'/Senior-Portfolio-Page/photo'} onClick={() => setNavExtended(!navExtended)} page='photo'>photo</Link>
+          <Link to={'/Senior-Portfolio-Page/about'} onClick={() => setNavExtended(!navExtended)} page='about'>about</Link>
+        </nav>
          </div>
          
         </>
